51edf3bbaa940beeef5b2c70c383c314e6faebdd
samagrawal2418 <sandeepagrawal2418@gmail.com>
2021-05-24 22:14:52 -0400

    commenting and refactor

--- src/com/bu/assignment/filemerger/MyFileRunner.java
+++ src/com/bu/assignment/filemerger/MyFileRunner.java
@@ -9,64 +9,86 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author Sandeep Agrawal
+ * This project is to show how we can merge 2 json files and search for keywords in the merged file and print the data from the searched object
+ */
 
 public class MyFileRunner {
 
+    //constants
     private static final String FILE_1 = "file1.json";
     private static final String FILE_2 = "file2.json";
     private static final String MERGED_FILE = "mergedFile.json";
 
-    public static void main(String[] args) {
+    /**
+     * @param args this contains the java command line arguments
+     * @return Does not return anything
+     * @description This method is the main method and is used to execute other
+     * methods in this file to read and write to new file and search from the merged file
+     */
+    public static void main(String[] args) throws IOException {
 
-        try {
-            readAndWriteToNewFile(FILE_1);
-            readAndWriteToNewFile(FILE_2);
-            readFromFile();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        readAndWriteToNewFile(FILE_1); //read file 1 and write to the merged file
+        readAndWriteToNewFile(FILE_2); //read file 2 and write to the merged file
+        searchKeywordsInFile(); //search defined keywords from the merged file
     }
 
+    /**
+     * @param fileName Passed filename from the main method to read the file
+     * @throws IOException This method will read the content of the passed filename and write the content to a new file
+     */
     public static void readAndWriteToNewFile(String fileName) throws IOException {
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
+        BufferedReader bufferedReader = null; //declare bufferedReader instance
+        BufferedWriter bufferedWriter = null; //declare bufferedWriter instance
+
         try {
-            bufferedReader = new BufferedReader(new FileReader(fileName));
-            bufferedWriter = new BufferedWriter(new FileWriter(MERGED_FILE, true));
-            String s;
-            if (bufferedReader != null) {
-                while ((s = bufferedReader.readLine()) != null) {
-                    if (bufferedWriter != null) {
-                        bufferedWriter.write(s + "\n");
+            bufferedReader = new BufferedReader(new FileReader(fileName)); //create bufferedReader object and filereader object with the filename
+            bufferedWriter = new BufferedWriter(new FileWriter(MERGED_FILE, true)); //create bufferedWriter object with the filewriter object with new file and set append to true
+            String s; //declare string variable to be used for reading and writing to the file
+            if (bufferedReader != null) { //check if bufferedReader is not null before accessing it
+                while ((s = bufferedReader.readLine()) != null) { //check if buffered reader line is not null
+                    if (bufferedWriter != null) { //check if buffered writer is not null before accessing object
+                        bufferedWriter.write(s + "\n"); //write to the bufferedWriter and add line break
                     }
                 }
             }
-
+            //catch Exceptions
         } catch (FileNotFoundException e) {
+            e.printStackTrace(); //print the details of exception
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (bufferedReader != null) {
+        } finally { //clean up all the resources
+            if (bufferedReader != null) { //check if bufferedReader is not null before cleaning up the resources
                 bufferedReader.close();
             }
-            if (bufferedWriter != null) {
+            if (bufferedWriter != null) {//check if bufferedWriter is not null before cleaning up the resources
                 bufferedWriter.close();
             }
         }
     }
 
-    public static void readFromFile() {
-        BufferedReader bufferedReader = null;
+    /**
+     * @return Does not return anything
+     * This method reads the merged file and searches for the keywords in each line
+     */
+    public static void searchKeywordsInFile() {
+        BufferedReader bufferedReader = null; //create bufferedReader instance
+
         try {
-            bufferedReader = new BufferedReader(new FileReader(MERGED_FILE));
-            String s;
-            while ((s = bufferedReader.readLine()) != null) {
-                searchForKeyword(s);
+            bufferedReader = new BufferedReader(new FileReader(MERGED_FILE)); //create bufferedReader object and pass the filereader object with filename
+            String s; //declare the string variable to use for reading the file
+            while ((s = bufferedReader.readLine()) != null) { //loop through each line and check when file is completed
+                searchForKeyword(s); //pass each line content to the search for keyword method
             }
+            //handle exceptions and print the stack trace
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        } finally {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally { //check bufferedReader is not null and cleanup resources
             if (bufferedReader != null) {
                 try {
                     bufferedReader.close();
@@ -77,36 +99,49 @@ public class MyFileRunner {
         }
     }
 
-    private static void searchForKeyword(String data) {
-
+    /**
+     * @param content Pass each line content to this method to search for the given keyword
+     * @return This method does not return anything
+     * @description This method is used to search for the keywords in each line using regex and if found a match then calls the different method
+     */
+    private static void searchForKeyword(String content) {
         //Regular expressions
         String regex1 = "fitness";
         String regex2 = "wearable";
         //Creating a pattern objects
         Pattern pattern1 = Pattern.compile(regex1);
         Pattern pattern2 = Pattern.compile(regex2);
-        //Creating an List object
+        //Creating a List object
         List<Pattern> patterns = new ArrayList<>();
+        //Add the patters to the list
         patterns.add(pattern1);
         patterns.add(pattern2);
+        //Loop through the pattern and check if keyword matches and call the convertStringToJsonObject with the content of the line
         for (Pattern pattern : patterns) {
-            Matcher matcher = pattern.matcher(data);
+            Matcher matcher = pattern.matcher(content);
             if (matcher.find()) {
-                convertStringToJsonObject(data);
+                convertStringToJsonObject(content);
             }
         }
     }
 
-    private static void convertStringToJsonObject(String data) {
+    /**
+     * @param content This is the content of the line passed from the previous method once there is match of keyword
+     * @return This method does not return anything
+     * @description This method is used to convert the string to json object and print the fund_raised_percent and the close_date from the JSONObject
+     */
+    private static void convertStringToJsonObject(String content) {
         try {
-            JSONObject jsonObject = new JSONObject(data);
-            JSONObject d = jsonObject.getJSONObject("data");
-            double funds_raised_percent = d.getDouble("funds_raised_percent");
-            String close_data = d.getString("close_date");
-            System.out.println("funds_raised_percent: " + funds_raised_percent + "\nclose_date: " + close_data + "\n");
-
+            JSONObject jsonObject = new JSONObject(content); //create JSONObject and pass the string
+            JSONObject data = jsonObject.getJSONObject("data"); //create JSONObject and get the JSONObject for the data field which holds the other data
+            double funds_raised_percent = data.getDouble("funds_raised_percent"); //using the data JSONObject and get the funds_raised_percent of double type
+            String close_data = data.getString("close_date"); //using the data JSONObject and get the close_date of string type
+            System.out.println("funds_raised_percent: " + funds_raised_percent + "\nclose_date: " + close_data + "\n"); //print the funds_raised percent and close date
+            //handle exception
         } catch (JSONException err) {
             err.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
